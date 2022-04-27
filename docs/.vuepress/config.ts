e1915819a05ab4a48bb733cd2649f3f46d1f6a36
Ahmad Kholid <Kholid060@gmail.com>
2022-04-27 11:32:40 +0800

    fix: theme config

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,44 +1,22 @@
 require('dotenv').config();
 import { defineUserConfig } from '@vuepress/cli';
 import { path } from '@vuepress/utils';
-import type { DefaultThemeOptions } from '@vuepress/theme-default';
+import { docsearchPlugin } from '@vuepress/plugin-docsearch';
 import { navbar, sidebar } from './configs';
+import { defaultTheme } from '@vuepress/theme-default';
 
-export default defineUserConfig<DefaultThemeOptions>({
-  theme: '@vuepress/theme-default',
-  dest: 'public',
-  head: [
-    ['link', { rel: 'icon', href: '/images/logo.png' }],
-  ],
-  clientAppEnhanceFiles: path.resolve(__dirname, './clientAppEnhance.ts'),
-  plugins: [
-    [
-      '@vuepress/plugin-docsearch',
-      {
-        apiKey: process.env.SEARCH_API_KEY,
-        indexName: process.env.SEARCH_INDEX_NAME,
-        appId: process.env.SEARCH_APP_ID,
-        locales: {
-          '/': {
-            placeholder: 'Search docs',
-            translations: {
-              button: {
-                buttonText: 'Search docs',
-              },
-            },
-          },
-        },
-      }
-    ]
-  ],
-  locales: {
-    '/': {
-      lang: 'en-US',
-      title: 'Automa Docs',
-      description: 'Documentation for Automa Chrome Extension',
-    },
-  },
-  themeConfig: {
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      SEARCH_API_KEY: string;
+      SEARCH_APP_ID: string;
+      SEARCH_INDEX_NAME: string;
+    }
+  }
+}
+
+export default defineUserConfig({
+  theme: defaultTheme({
     docsDir: 'docs',
     themePlugins: {
       mediumZoom: true,
@@ -54,5 +32,36 @@ export default defineUserConfig<DefaultThemeOptions>({
     },
     logo: '/images/logo.png',
     repo: 'kholid060/automa-docs',
+  }),
+  dest: 'public',
+  head: [
+    ['link', { rel: 'icon', href: '/images/logo.png' }],
+  ],
+  clientAppEnhanceFiles: path.resolve(__dirname, './clientAppEnhance.ts'),
+  plugins: [
+    [
+      docsearchPlugin({
+        apiKey: process.env.SEARCH_API_KEY,
+        appId: process.env.SEARCH_APP_ID,
+        indexName: process.env.SEARCH_INDEX_NAME,
+        locales: {
+          '/': {
+            placeholder: 'Search docs',
+            translations: {
+              button: {
+                buttonText: 'Search docs',
+              },
+            },
+          },
+        },
+      })
+    ]
+  ],
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: 'Automa Docs',
+      description: 'Documentation for Automa Chrome Extension',
+    },
   },
 });
